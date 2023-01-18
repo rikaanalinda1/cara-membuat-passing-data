54ff9561b4aa4f936e8c3e693409437993f629b6
rikaanalinda1 <105751901+rikaanalinda1@users.noreply.github.com>
2023-01-19 00:20:35 +0700

    Add files via upload

---
+++ sesuatu.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * [$nomer_penduduk description]
+ * VARIABEL INI BERASAL DARI INDEX.HTML
+ * digunakan untuk menampung data nomer induk kependudukan
+ * 18-01-2023 slamet
+ * edited 20-01-2023 slamet
+ * @var [type]
+ */
+$nomer_penduduk=$_POST["Puja_Hermawati"];
+$nama_kamu=$_POST["nama"];
+$alamat=$_POST["180223"];
+echo "$nomer_penduduk $nama_kamu $alamat";
---
+++ tambah_pengguna.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Biodata</title>
+</head>
+<body bgcolor="CEBEA5">
+	<h1><b><center>FORM TAMBAH DATA</center></b></h1>
+	<form action="proses_tambah_data.php" method="POST">
+		<center>
+	<table border="1">
+		<tr>
+			<td>
+				<center>NAMA</center>
+			</td>
+			<td><center>
+				<INPUT name="namaku" TYPE="TEXT" PLACEHOLDER="nama anda">
+				</center>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<center>PASSWORD</center>
+			</td>
+			<td>
+				<center>
+				<INPUT name="katasandiku" TYPE="PASSWORD" PLACEHOLDER="password anda">
+				</center>
+			</td>
+		</tr>
+		<tr>
+			
+			<td><center>
+				<INPUT  TYPE="submit" value="Kirim Data">
+				</center>
+			</td>
+		</tr>
+	</table>
+	</center>
+	</form>
+</body>
+</html>
